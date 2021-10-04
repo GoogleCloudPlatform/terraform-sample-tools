7aa07de7d727bb17e02b7b35ae66fe4a1ef1cf11
Betsy Lichtenberg <betsyl@google.com>
2021-10-04 14:21:23 -0700

    Update convert2erb.py

--- convert2erb.py
+++ convert2erb.py
@@ -22,7 +22,7 @@ Job of this script to convert .tf into .erb & .yaml file.
 The following output files are generated from the location where script is
 executed:
 
-*  An .erb file that ou can add to Magic Modules in mmv1/templates/terraform/examples.
+*  An .erb file that you can add to Magic Modules in mmv1/templates/terraform/examples.
    The output .erb file will have same name as the .tf file
    (for test.tf, output file is test.tf.erb).
 *  A terraform.yaml_check file contains content that you can add to Magic Modules in
