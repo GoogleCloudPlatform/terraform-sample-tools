10dbae7386758b749b7bf710e5c54b0f7f0d0b57
Sampath M <sampathm@google.com>
2022-08-31 20:20:50 +0200

    Update helper file

--- tools/magic-modules-tftools/run.sh
+++ tools/magic-modules-tftools/run.sh
@@ -1,10 +1,43 @@
 #!/bin/bash
+cleanup () {
+  # Clear MAC OS cache files
+  find . | grep -E "(.DS_Store)" | xargs rm -rf
+  # Clearn Python cache files
+  find . | grep -E "(/__pycache__$|\.pyc$|\.pyo$)" | xargs rm -rf
+  rm -rf dist/ build/ .nox/ .pytest_cache/ tftools.egg-info/ tests/sponge_log.xml
+  rm -rf test/sponge_log.xml test/__pycache__ __pycache__
+  clear
+  echo "Cleanup completed"
+}
 
-# Clear logs & build files
-rm -rf dist/ build/ .nox/ .pytest_cache/ tftools.egg-info/ tests/sponge_log.xml
+update_dockerhub_image () {
+  cleanup
+  docker build -t tftools:latest .
+  docker tag tftools:latest msampathkumar/tftools:`date +'%Y%m%d'`
+  docker tag tftools:latest msampathkumar/tftools:latest
+  docker push msampathkumar/tftools:latest
+  docker push tftools:latest msampathkumar/tftools:`date +'%Y%m%d'`
+}
 
-rm test/sponge_log.xml test/__pycache__ __pycache__
+install_tftools () {
+  cleanup
+  python3 setup.py install
+}
 
-python3 setup.py install
+#############################################################
+####### From CLI, you can run the following commands` #######
+#############################################################
+#. ./run.sh && cleanup
+#. ./run.sh && update_dockerhub_image
+#. ./run.sh && install_tftools
 
-rm -rf dist/ build/ .nox/ .pytest_cache/ tftools.egg-info/
+# install_tftools
+
+# basic test
+#cd tests
+#pytest -v
+#cd ..
+
+# final test: multi-version test
+ nox
+
